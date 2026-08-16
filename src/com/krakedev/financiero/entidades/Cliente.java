dacc375022c2ef5e20d9424da88917cbaef6b5ee
Alexandra Ramirez <133824521+alexandraR-Code@users.noreply.github.com>
2026-08-15 19:28:56 -0500

    Feat:Agregar clase Cliente con atributos, metodos getter, setter, imprimir y un constructor que recibe 3 parametros

---
+++ src/com/krakedev/financiero/entidades/Cliente.java
@@ -0,0 +1,51 @@
+package com.krakedev.financiero.entidades;
+
+public class Cliente {
+
+	// atributos
+	private String cedula;
+	private String nombre;
+	private String apellido;
+
+	// constructor
+	public Cliente(String cedula, String nombre, String apellido) {
+		this.cedula = cedula;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	// Metodos setter y getter
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	// Metodo imprimir
+	
+	public void imprimir() {
+		System.out.println("Cedula: " + this.getCedula());
+		System.out.println("Nombre: " + this.getNombre());
+		System.out.println("Apellido: " + this.getApellido());
+	}
+
+}
