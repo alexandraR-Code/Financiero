df0bb2dc7a7fc5ce8030522201d670b6973f50e0
Alexandra Ramirez <133824521+alexandraR-Code@users.noreply.github.com>
2026-08-15 19:18:15 -0500

    feat: creacion de paquete com.krakedev.financiero.entidades, creacion de clases cuenta con atributos, metodos getter y setter, metodo imprimir y un constructor con un parametro

---
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -0,0 +1,57 @@
+package com.krakedev.financiero.entidades;
+
+public class Cuenta {
+
+	// atributos
+	private String id;
+	private double saldoActual;
+	private String tipo;
+
+	// Constructor
+
+	public Cuenta(String id) {
+		this.id = id;
+		this.saldoActual = 0;
+		this.tipo = "A";
+	}
+
+	// Metodos getter y setter
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public double getSaldoActual() {
+		return saldoActual;
+	}
+
+	public void setSaldoActual(double saldoActual) {
+		this.saldoActual = saldoActual;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	//Metodo imprimir
+	public void imprimir() {
+		System.out.println("Id: " + this.getId());
+		System.out.println("Saldo actual: " + this.getSaldoActual());
+		System.out.println("Tipo de cuenta: " + this.getTipo());
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
